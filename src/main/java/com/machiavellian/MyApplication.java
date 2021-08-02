d17f715f0ff8c20d802f2df8820ade84f49105e8
mfyang1218 <87477080+mfyang1218@users.noreply.github.com>
2021-08-02 12:28:36 +0800

    Create MyApplication.java

---
+++ src/main/java/com/machiavellian/MyApplication.java
@@ -0,0 +1,22 @@
+package com.machiavellian;
+
+
+public class MyApplication  {
+
+	public Integer add(Integer firstArg, Integer secondArg) {
+		return firstArg + secondArg;
+	}
+
+    public Integer subtract(Integer firstArg, Integer secondArg) {
+        return firstArg - secondArg;
+    }
+
+    public Integer multiply(Integer firstArg, Integer secondArg) {
+        return firstArg * secondArg;
+    }
+
+    public Integer divide(Integer firstArg, Integer secondArg) {
+        return firstArg / secondArg;
+    }
+	
+}
